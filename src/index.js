2d1f709766e187d43494d91e2d5eea1714db84be
Swaraj <swarajgosavi20@gmail.com>
2023-03-11 17:47:31 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app'
 
 import {
-    getDatabase, ref, set
+    getDatabase, ref, set, onValue, get, child
 }from "firebase/database"
 
 const firebaseConfig = {
@@ -20,15 +20,26 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 
 //init database
-function wirteUserData(userId, name, email, password) {
-    const db = getDatabase();
-    const referance = ref(db, 'users/' + userId);  
+// function wirteUserData(userId, name, email, password) {
+//     const db = getDatabase();
+//     const referance = ref(db, 'users/' + userId);  
 
-    set(referance, {
-        username: name,
-        email: email,
-        password: password
-    });
-}
+//     set(referance, {
+//         username: name,
+//         email: email,
+//         password: password
+//     });
+// }
 
-wirteUserData("Joel", "joel", "joelalph@yahoo.com", "12345");
+// wirteUserData("Joel", "joel", "joelalph@yahoo.com", "12345");
+
+const dbRef = ref(getDatabase());
+get(child(dbRef, `users/${userId=Swaraj}`)).then((snapshot) => {
+  if (snapshot.exists()) {
+    console.log(snapshot.val());
+  } else {
+    console.log("No data available");
+  }
+}).catch((error) => {
+  console.error(error);
+});
