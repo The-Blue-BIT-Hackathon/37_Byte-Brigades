42f90431de59ce4647769ef0aef04ddd26ac217d
Swaraj <swarajgosavi20@gmail.com>
2023-03-11 15:49:34 +0530

    Added real time data

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import { initializeApp } from 'firebase/app'
+
+import {
+    getDatabase, ref, set
+}from "firebase/database"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDTbVXdbcNdfLM4bninNCNkw_qHhOcu5es",
+    authDomain: "mess-3a90c.firebaseapp.com",
+    databaseURL: "https://mess-3a90c-default-rtdb.firebaseio.com",
+    projectId: "mess-3a90c",
+    storageBucket: "mess-3a90c.appspot.com",
+    messagingSenderId: "267416052298",
+    appId: "1:267416052298:web:af4c6431b93903f60c3cb3"
+  };
+
+
+  
+// init firebase app
+const app = initializeApp(firebaseConfig)
+
+//init database
+function wirteUserData(userId, name, email, password) {
+    const db = getDatabase();
+    const referance = ref(db, 'users/' + userId);  
+
+    set(referance, {
+        username: name,
+        email: email,
+        password: password
+    });
+}
+
+wirteUserData("Joel", "joel", "joelalph@yahoo.com", "12345");
